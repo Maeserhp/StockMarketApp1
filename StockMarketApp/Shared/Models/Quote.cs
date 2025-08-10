fcc5cf29aba661a54381e0d7ade9e70d800d14d2
Maeserhp <maeserhp@gmail.com>
2025-08-09 21:24:58 -0600

    Created shared project

---
+++ StockMarketApp/Shared/Models/Quote.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Models
+{
+    public class Quote
+    {
+        [JsonPropertyName("c")]
+        public decimal CurrentPrice { get; set; }
+
+        [JsonPropertyName("h")]
+        public decimal HighPrice { get; set; }
+
+        [JsonPropertyName("l")]
+        public decimal LowPrice { get; set; }
+
+        [JsonPropertyName("o")]
+        public decimal OpenPrice { get; set; }
+
+        [JsonPropertyName("pc")]
+        public decimal PreviousClosePrice { get; set; }
+    }
+}
---
+++ StockMarketApp/Shared/Models/StockSymbol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    internal class StockSymbol
+    {
+        public string Currency { get; set; } 
+        public string Description { get; set; } 
+        public string DisplaySymbol { get; set; } 
+        public string Figi { get; set; } 
+        public string Mic { get; set; } 
+        public string Symbol { get; set; } 
+        public string Type { get; set; } 
+    }
+}
