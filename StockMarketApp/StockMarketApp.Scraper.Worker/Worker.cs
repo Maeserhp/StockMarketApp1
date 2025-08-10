11ef0d22319b896a1675b1b5d4308921a965b780
Maeserhp <maeserhp@gmail.com>
2025-08-09 21:26:26 -0600

    Successfully get data from Finhubb api

--- StockMarketApp/StockMarketApp.Scraper.Worker/Worker.cs
+++ StockMarketApp/StockMarketApp.Scraper.Worker/Worker.cs
@@ -1,8 +1,16 @@
+using HtmlAgilityPack;
+using Shared.Models;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace StockMarketApp.Scraper.Worker
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+
+        private string apiKey = "d2bttn9r01qvh3vdaei0d2bttn9r01qvh3vdaeig";
 
         public Worker(ILogger<Worker> logger)
         {
@@ -11,16 +19,38 @@ namespace StockMarketApp.Scraper.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                if (_logger.IsEnabled(LogLevel.Information))
-                {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            //NOTE: Enable this loop if you want the process to loop
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
 
-                    _logger.LogInformation("My custom log!!!!!!!!!!!!!!!!");
-                }
-                await Task.Delay(10000, stoppingToken); // wait 10 second
+            //TODO: Store this list of symbols in CosmosDB and read them in here.
+            var stocksToGet = new List<string>() { "AAPL", "TSLA", "MMM", "CVX", "KO"};
+
+            foreach (var stock in stocksToGet)
+            {
+                await GetFinnhubQuoteAsync(stock);
             }
+
+
+            //await Task.Delay(10000, stoppingToken); // wait 10 second before restarting the loop
+            //}
+        }
+
+        public async Task GetFinnhubQuoteAsync(string symbol)
+        {
+            string apiKey = "d2bttn9r01qvh3vdaei0d2bttn9r01qvh3vdaeig";
+            string url = $"https://finnhub.io/api/v1/quote?symbol={symbol}&token={apiKey}";
+
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var quote = JsonSerializer.Deserialize<Quote>(json);
+            //TODO: Save results to CosmosDB
+            _logger.LogInformation($"Current price for {symbol}: {quote.CurrentPrice}");
         }
     }
 }
